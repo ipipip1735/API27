c9bf59ae89ca43523090b0b7af4b794b45f8e864
ipipip1735 <ipipip1735@163.com>
2018-10-10 17:36:04 +0800

    Add FileProvider

--- fileprovider/src/main/java/mine/fileprovider/MainActivity.java
+++ fileprovider/src/main/java/mine/fileprovider/MainActivity.java
@@ -13,9 +13,13 @@ import java.io.InputStreamReader;
 import java.nio.CharBuffer;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.FileVisitResult;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.stream.Stream;
 
 import static java.nio.file.StandardOpenOption.APPEND;
 import static java.nio.file.StandardOpenOption.CREATE;
@@ -117,36 +121,66 @@ public class MainActivity extends AppCompatActivity {
     public void read(View view) {
         System.out.println("~~button.read~~");
 
+
+        //遍历打印Files目录下的所有目录
+//        try {
+//
+//            Path path = Paths.get(getFilesDir().toString());
+//            Files.walkFileTree(path, new SimpleFileVisitor() {
+//                @Override
+//                public FileVisitResult preVisitDirectory(Object dir, BasicFileAttributes attrs) throws IOException {
+//                    Path parent = Paths.get(getFilesDir().toString());
+//                    Path relative = parent.relativize((Path) dir);
+//
+//                    System.out.println("relative path is " + relative);
+//                    return super.preVisitDirectory(dir, attrs);
+//                }
+//            });
+//
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+
+
+
+        //方法一：读取文件，使用BufferedReader
+//        try {
+//
+//            Path path = Paths.get(getFilesDir().toString(), "Logs", "sql.log");
+//            System.out.println("path is " + path);
+//
+//            BufferedReader bufferedReader = Files.newBufferedReader(path, StandardCharsets.UTF_8);
+//
+//            StringBuilder stringBuilder = new StringBuilder(128);
+//            CharBuffer buffer = CharBuffer.allocate(1024);
+//            while (bufferedReader.read(buffer) != -1) {
+//                buffer.flip();
+//                while (buffer.hasRemaining()) {
+//                    stringBuilder.append(buffer.get());
+//                }
+//                buffer.clear();
+//            }
+//
+//            bufferedReader.close();
+//
+//            System.out.println(stringBuilder.toString());
+//
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+
+        //方法e二：读取文件，使用Stream对象
         try {
 
             Path path = Paths.get(getFilesDir().toString(), "Logs", "sql.log");
             System.out.println("path is " + path);
+            Stream<String> stringStream = Files.lines(path);
+            stringStream.forEach(System.out::println);
 
-            BufferedReader bufferedReader = Files.newBufferedReader(path, StandardCharsets.UTF_8);
-
-            StringBuilder stringBuilder = new StringBuilder(128);
-            CharBuffer buffer = CharBuffer.allocate(1024);
-            while (bufferedReader.read(buffer) != -1) {
-                buffer.flip();
-                while (buffer.hasRemaining()) {
-                    stringBuilder.append(buffer.get());
-                }
-                buffer.clear();
-            }
-
-            bufferedReader.close();
-
-            System.out.println(stringBuilder.toString());
 
         } catch (IOException e) {
             e.printStackTrace();
         }
-
-
-
-
-
-
 
 
     }
