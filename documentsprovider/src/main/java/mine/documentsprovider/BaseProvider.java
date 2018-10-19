c29921ef02798daa6fcc6290884b405c6fe9c80a
ipipip1735 <ipipip1735@163.com>
2018-10-19 16:12:17 +0800

    Update DocumentProvider

--- documentsprovider/src/main/java/mine/documentsprovider/BaseProvider.java
+++ documentsprovider/src/main/java/mine/documentsprovider/BaseProvider.java
@@ -30,6 +30,8 @@ public class BaseProvider extends DocumentsProvider {
             DocumentsContract.Document.COLUMN_LAST_MODIFIED};
 
 
+    final String ROOT = "/";
+
     public BaseProvider() {
         System.out.println("*********  BaseProvider.Constructor  *********");
     }
@@ -41,10 +43,12 @@ public class BaseProvider extends DocumentsProvider {
         final MatrixCursor result = new MatrixCursor(DEFAULT_ROOT_PROJECTION);
 
         final MatrixCursor.RowBuilder row = result.newRow();
-        row.add(DocumentsContract.Root.COLUMN_ROOT_ID, 1)
+        row.add(DocumentsContract.Root.COLUMN_ROOT_ID, ROOT)
                 .add(DocumentsContract.Root.COLUMN_TITLE, "rootTitle")
-                .add(DocumentsContract.Root.COLUMN_FLAGS, DocumentsContract.Root.FLAG_SUPPORTS_CREATE | DocumentsContract.Root.FLAG_SUPPORTS_RECENTS | DocumentsContract.Root.FLAG_SUPPORTS_SEARCH)
-                .add(DocumentsContract.Root.COLUMN_SUMMARY, "rootSummary");
+                .add(DocumentsContract.Root.COLUMN_FLAGS, DocumentsContract.Root.FLAG_SUPPORTS_CREATE | DocumentsContract.Root.FLAG_SUPPORTS_RECENTS | DocumentsContract.Root.FLAG_SUPPORTS_SEARCH )
+                .add(DocumentsContract.Root.COLUMN_MIME_TYPES, DocumentsContract.Root.MIME_TYPE_ITEM)
+                .add(DocumentsContract.Root.COLUMN_SUMMARY, "rootSummary")
+                .add(DocumentsContract.Root.COLUMN_DOCUMENT_ID, ROOT);
 
         return result;
     }
@@ -53,21 +57,74 @@ public class BaseProvider extends DocumentsProvider {
     public Cursor queryDocument(String documentId, String[] projection) throws FileNotFoundException {
         System.out.println("*********  " + getClass().getSimpleName() + ".queryDocument  *********");
 
+        System.out.println("documentId is " + documentId);
 
-        return null;
+        MatrixCursor result = new MatrixCursor(DEFAULT_DOCUMENT_PROJECTION);
+
+
+        switch (documentId) {
+            case ROOT:
+                MatrixCursor.RowBuilder row = result.newRow();
+                row.add(DocumentsContract.Document.COLUMN_DOCUMENT_ID, documentId + "zero/")
+                        .add(DocumentsContract.Document.COLUMN_DISPLAY_NAME, "0")
+                        .add(DocumentsContract.Document.COLUMN_FLAGS, DocumentsContract.Document.FLAG_SUPPORTS_WRITE)
+                        .add(DocumentsContract.Document.COLUMN_MIME_TYPE, DocumentsContract.Document.MIME_TYPE_DIR);
+
+                break;
+
+//            case ROOT:
+//                MatrixCursor.RowBuilder row = result.newRow();
+//                row.add(DocumentsContract.Document.COLUMN_DOCUMENT_ID, documentId + "zero.jpg")
+//                        .add(DocumentsContract.Document.COLUMN_DISPLAY_NAME, "0")
+//                        .add(DocumentsContract.Document.COLUMN_MIME_TYPE, "image/jpg");
+//
+//                break;
+
+//            case ROOT + "zero/" + "1f0":
+//                row.add(DocumentsContract.Document.COLUMN_DOCUMENT_ID, documentId + "zero/") .add(DocumentsContract.Document.COLUMN_DISPLAY_NAME, "0")
+//                        .add(DocumentsContract.Document.COLUMN_MIME_TYPE, DocumentsContract.Document.MIME_TYPE_DIR);
+//
+//                break;
+
+            default:
+                System.out.println("there isn't a vaild file");
+        }
+        return result;
     }
 
     @Override
     public Cursor queryChildDocuments(String parentDocumentId, String[] projection, String sortOrder) throws FileNotFoundException {
         System.out.println("*********  " + getClass().getSimpleName() + ".queryChildDocuments  *********");
 
-        final MatrixCursor result = new
-                MatrixCursor(DEFAULT_DOCUMENT_PROJECTION);
-        final File parent = getFileForDocId(parentDocumentId);
-        for (File file : parent.listFiles()) {
-            // Adds the file&#39;s display name, MIME type, size, and so on.
-            includeFile(result, null, file);
+        System.out.println("parentDocumentId is " + parentDocumentId);
+
+        MatrixCursor result = new MatrixCursor(DEFAULT_DOCUMENT_PROJECTION);
+
+        switch (parentDocumentId) {
+
+            case ROOT + "zero/":
+                result.newRow().add(DocumentsContract.Document.COLUMN_DOCUMENT_ID, parentDocumentId + "0f0")
+                        .add(DocumentsContract.Document.COLUMN_DISPLAY_NAME, "1f0.txt")
+                        .add(DocumentsContract.Document.COLUMN_MIME_TYPE, "text/plain")
+                .add(DocumentsContract.Document.COLUMN_MIME_TYPE, "text/plain");
+                result.newRow().add(DocumentsContract.Document.COLUMN_DOCUMENT_ID, parentDocumentId + "0f1")
+                        .add(DocumentsContract.Document.COLUMN_DISPLAY_NAME, "1f1.txt")
+                        .add(DocumentsContract.Document.COLUMN_MIME_TYPE, "text/plain");
+                result.newRow().add(DocumentsContract.Document.COLUMN_DOCUMENT_ID, parentDocumentId + "one/")
+                        .add(DocumentsContract.Document.COLUMN_DISPLAY_NAME, "d1")
+                        .add(DocumentsContract.Document.COLUMN_MIME_TYPE, DocumentsContract.Document.MIME_TYPE_DIR);
+                break;
+
+            case ROOT + "zero/" + "one/" :
+                result.newRow().add(DocumentsContract.Document.COLUMN_DOCUMENT_ID, parentDocumentId + "1f0")
+                        .add(DocumentsContract.Document.COLUMN_DISPLAY_NAME, "1f0.jpg")
+                        .add(DocumentsContract.Document.COLUMN_MIME_TYPE, "image/jpg");
+                break;
+
+            default:
+                System.out.println("there isn't a vaild file");
         }
+
         return result;
     }
 
