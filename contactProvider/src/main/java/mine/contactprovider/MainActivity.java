9441a8d0805cac879d50a82779d6e2a00a9d34a3
ipipip1735 <ipipip1735@163.com>
2018-11-04 17:31:04 +0800

    Update ContactProvider

--- contactProvider/src/main/java/mine/contactprovider/MainActivity.java
+++ contactProvider/src/main/java/mine/contactprovider/MainActivity.java
@@ -215,8 +215,8 @@ public class MainActivity extends AppCompatActivity {
 //        queryProfile();
 //        queryContact();
 //        queryRawContact();
-        queryData();
-//        queryWithEtity();
+//        queryData();
+        queryWithEtity();
 //        queryPhoneLookup();
     }
 
@@ -250,14 +250,16 @@ public class MainActivity extends AppCompatActivity {
 
     private void queryWithEtity() {
         //Contacts实体
-        int rawContactId = 2;
-        Uri rawContactUri = ContentUris.withAppendedId(ContactsContract.Contacts.CONTENT_URI, rawContactId);
-        Uri entityUri = Uri.withAppendedPath(rawContactUri, ContactsContract.Contacts.Entity.CONTENT_DIRECTORY);
+        int contactId = 2;
+        Uri contactUri = ContentUris.withAppendedId(
+                ContactsContract.Contacts.CONTENT_URI, contactId);
+        Uri entityUri = Uri.withAppendedPath(
+                contactUri, ContactsContract.Contacts.Entity.CONTENT_DIRECTORY);
         String[] projection = new String[]{
                 ContactsContract.Contacts.Entity.DATA_ID};
 
         //Raw_Contacts实体
-//        int rawContactId = 1;
+//        int rawContactId = 2;
 //        Uri rawContactUri = ContentUris.withAppendedId(
 //                ContactsContract.RawContacts.CONTENT_URI, rawContactId);
 //        Uri entityUri = Uri.withAppendedPath(rawContactUri,
@@ -265,9 +267,8 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-
-
-        Cursor cursor = getContentResolver().query(entityUri, null, null, null, null);
+        Cursor cursor = getContentResolver().query(entityUri,
+                null, null, null, null);
 
 
         if (Objects.isNull(cursor)) return;
