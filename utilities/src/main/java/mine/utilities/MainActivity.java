49f100fc46ee0793c6741ae75ff42766df5b8d17
ipipip1735 <ipipip1735@163.com>
2018-11-30 08:17:09 +0800

    Add Utilities

---
+++ utilities/src/main/java/mine/utilities/MainActivity.java
@@ -0,0 +1,123 @@
+package mine.utilities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.LruCache;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        System.out.println("*********  " + getClass().getSimpleName() + ".onStart  *********");
+        setContentView(R.layout.activity_main);
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        System.out.println("*********  " + getClass().getSimpleName() + ".onStart  *********");
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        System.out.println("*********  " + getClass().getSimpleName() + ".onRestoreInstanceState  *********");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        System.out.println("*********  " + getClass().getSimpleName() + ".onRestart  *********");
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        System.out.println("*********  " + getClass().getSimpleName() + ".onResume  *********");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        System.out.println("*********  " + getClass().getSimpleName() + ".onPause  *********");
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        System.out.println("*********  " + getClass().getSimpleName() + ".onBackPressed  *********");
+    }
+
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        System.out.println("*********  " + getClass().getSimpleName() + ".onStop  *********");
+    }
+
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        System.out.println("*********  " + getClass().getSimpleName() + ".onSaveInstanceState  *********");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        System.out.println("*********  " + getClass().getSimpleName() + ".onDestroy  *********");
+    }
+
+
+    public void start(View view) {
+        System.out.println("~~button.start~~");
+
+
+        int cacheSize = 4 * 1024 * 1024; // 4MiB
+        LruCache<String, Integer> bitmapCache = new LruCache<String, Integer>(cacheSize) {
+            protected int sizeOf(String key,  Integer value) {
+                return 4;
+            }
+        };
+
+
+    }
+
+
+    public void stop(View view) {
+        System.out.println("~~button.stop~~");
+
+    }
+
+    public void bind(View view) {
+        System.out.println("~~button.bind~~");
+
+    }
+
+    public void unbind(View view) {
+        System.out.println("~~button.unbind~~");
+
+    }
+
+    public void reloading(View view) {
+        System.out.println("~~button.reloading~~");
+
+    }
+
+
+    public void del(View view) {
+        System.out.println("~~button.del~~");
+
+    }
+
+
+    public void query(View view) {
+        System.out.println("~~button.query~~");
+
+    }
+}
