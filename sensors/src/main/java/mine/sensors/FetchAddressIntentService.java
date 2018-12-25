250dba3f59a739178b806877f2a3e561e5ef9200
ipipip1735 <ipipip1735@163.com>
2018-12-25 17:21:14 +0800

    Update Sensors

---
+++ sensors/src/main/java/mine/sensors/FetchAddressIntentService.java
@@ -0,0 +1,23 @@
+package mine.sensors;
+
+import android.app.IntentService;
+import android.content.Intent;
+
+/**
+ * Created by Administrator on 2018/12/25.
+ */
+public class FetchAddressIntentService extends IntentService {
+    /**
+     * Creates an IntentService.  Invoked by your subclass's constructor.
+     *
+     * @param name Used to name the worker thread, important only for debugging.
+     */
+    public FetchAddressIntentService(String name) {
+        super(name);
+    }
+
+    @Override
+    protected void onHandleIntent(@androidx.annotation.Nullable Intent intent) {
+
+    }
+}
--- sensors/src/main/java/mine/sensors/MainActivity.java
+++ sensors/src/main/java/mine/sensors/MainActivity.java
@@ -1,36 +1,71 @@
 package mine.sensors;
 
+import android.content.IntentSender;
 import android.location.Location;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.google.android.gms.common.api.ResolvableApiException;
 import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationAvailability;
+import com.google.android.gms.location.LocationCallback;
 import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.location.LocationSettingsRequest;
 import com.google.android.gms.location.LocationSettingsResponse;
 import com.google.android.gms.location.SettingsClient;
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 
 import java.lang.reflect.Method;
 import java.util.Objects;
 
+/**
+ * Created by Administrator on 2018/12/24.
+ */
 public class MainActivity extends AppCompatActivity {
     TextView textView;
     FusedLocationProviderClient mFusedLocationClient;
+    LocationCallback mLocationCallback;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        System.out.println("*********  " + getClass().getSimpleName() + ".onStart  *********");
+        System.out.println("*********  " + getClass().getSimpleName() + ".onCreate  *********");
         setContentView(R.layout.activity_main);
         ViewGroup group = findViewById(R.id.fl);
         textView = new TextView(this);
         group.addView(textView);
+
+        mLocationCallback = new LocationCallback() {
+            @Override
+            public void onLocationAvailability(LocationAvailability locationAvailability) {
+                super.onLocationAvailability(locationAvailability);
+                System.out.println("~onLocationAvailability~");
+                System.out.println(locationAvailability);
+            }
+
+            @Override
+            public void onLocationResult(LocationResult locationResult) {
+                super.onLocationResult(locationResult);
+                System.out.println("~onLocationResult~");
+                if (locationResult == null) {
+                    System.out.println("null");
+                    return;
+                }
+                for (Location location : locationResult.getLocations()) {
+                    System.out.println("location is " + location);
+                    textView.setText("location is " + location);
+                }
+
+            }
+        };
 
     }
 
@@ -75,6 +110,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onStop() {
         super.onStop();
         System.out.println("*********  " + getClass().getSimpleName() + ".onStop  *********");
+        mFusedLocationClient.removeLocationUpdates(mLocationCallback);
     }
 
 
@@ -124,17 +160,13 @@ public class MainActivity extends AppCompatActivity {
 
         setting();
 
-
-//        LocationRequest locationRequest = LocationRequest.create();
-//        locationRequest.setInterval(10000);
-//        locationRequest.setFastestInterval(5000);
-//        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
-
     }
 
     private void setting() {
         LocationRequest locationRequest = LocationRequest.create();
-
+        locationRequest.setInterval(1000);
+        locationRequest.setFastestInterval(500);
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
         LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder()
                 .addLocationRequest(locationRequest);
 
@@ -145,17 +177,47 @@ public class MainActivity extends AppCompatActivity {
             public void onSuccess(LocationSettingsResponse locationSettingsResponse) {
                 System.out.println("~~onSuccess~~");
                 System.out.println("locationSettingsResponse is " + locationSettingsResponse);
+                System.out.println("status is " + locationSettingsResponse.getLocationSettingsStates());
 
                 if (locationSettingsResponse != null) {
                     textView.setText("locationSettingsResponse is " + locationSettingsResponse);
+                    textView.setText("status is " + locationSettingsResponse.getLocationSettingsStates());
                 }
             }
+        }).addOnFailureListener(this, new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                System.out.println("~~onFailure~~");
+                if (e instanceof ResolvableApiException) {
+                    try {
+                        ResolvableApiException resolvable = (ResolvableApiException) e;
+                        resolvable.startResolutionForResult(MainActivity.this, 11);
+                    } catch (IntentSender.SendIntentException sendEx) {
+                        // Ignore the error.
+                    }
+                }
+
+
+            }
         });
+
+
     }
 
     public void bind(View view) {
         System.out.println("~~button.bind~~");
 
+        if (Objects.isNull(mFusedLocationClient))
+            mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+
+        LocationRequest locationRequest = LocationRequest.create();
+        locationRequest.setInterval(10000);
+        locationRequest.setFastestInterval(5000);
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder()
+                .addLocationRequest(locationRequest);
+
+        mFusedLocationClient.requestLocationUpdates(locationRequest, mLocationCallback, null);
     }
 
     public void unbind(View view) {
@@ -172,6 +234,7 @@ public class MainActivity extends AppCompatActivity {
     public void del(View view) {
         System.out.println("~~button.del~~");
 
+
     }
 
 
@@ -186,5 +249,6 @@ public class MainActivity extends AppCompatActivity {
             System.out.print(method.getName());
         }
     }
+
 
 }
