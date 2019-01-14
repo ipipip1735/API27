0881a819c0c6cc1991b70e7ee3934f34061ac73b
ipipip1735 <ipipip1735@163.com>
2019-01-14 08:07:30 +0800

    Update Services

--- services/src/main/java/mine/services/ForegroundActivity.java
+++ services/src/main/java/mine/services/ForegroundActivity.java
@@ -1,5 +1,9 @@
 package mine.services;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.ComponentName;
 import android.content.Intent;
 import android.content.ServiceConnection;
@@ -8,6 +12,7 @@ import android.os.Handler;
 import android.os.IBinder;
 import android.os.Message;
 import android.os.Messenger;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
@@ -90,65 +95,37 @@ public class ForegroundActivity extends AppCompatActivity {
     public void start(View view) {
         System.out.println("~~start~~");
 
-        //启动服务
         Intent intent = new Intent(this, ForegroundService.class);
+        intent.putExtra("category", 0);
         startService(intent);
+
 
     }
 
     public void stop(View view) {
         System.out.println("~~stop~~");
-        Intent intent = new Intent(this, BaseService.class);
-        intent.setAction("bs");
+        Intent intent = new Intent(this, ForegroundService.class);
         stopService(intent);
 
     }
 
     public void bind(View view) {
         System.out.println("~~bind~~");
-
-        Intent intent = new Intent(this, BaseService.class);
-        intent.setAction("bs");
-
-        if (Objects.isNull(serviceConnection))
-            serviceConnection = new ServiceConnection() {
-                @Override
-                public void onServiceConnected(ComponentName componentName, IBinder iBinder) {
-                    System.out.println("..onServiceConnected..");
-                    System.out.println("componentName is " + componentName);
-                    System.out.println("iBinder is " + iBinder);
-
-                    //发送信息给服务端
-                    BaseBinder baseBinder = (BaseBinder) iBinder; //强制转换
-                    Handler serviceHandler = baseBinder.getServiceHandler();
-                    Message message = Message.obtain(null, 2);
-
-//                    message.replyTo = new Messenger(iBinder); //方式一
-                    message.replyTo = new Messenger(new Handler(new Handler.Callback() { //方式二
-                        @Override
-                        public boolean handleMessage(Message msg) {
-                            System.out.println("Clinet|" + msg.what);
-                             return true;
-                        }
-                    }));
-//
-                    serviceHandler.handleMessage(message);
-                }
-
-                @Override
-                public void onServiceDisconnected(ComponentName componentName) {
-                    System.out.println("..onServiceDisconnected..");
-                    System.out.println("componentName is " + componentName);
-                }
-            };
-        bindService(intent, serviceConnection, BIND_AUTO_CREATE);
-
     }
 
 
     public void unbind(View view) {
         System.out.println("~~unbind~~");
         if (Objects.nonNull(serviceConnection)) unbindService(serviceConnection);
+
+    }
+
+    public void del(View view) {
+        System.out.println("~~del~~");
+
+        Intent intent = new Intent(this, ForegroundService.class);
+        intent.putExtra("category", 1);
+        startService(intent);
 
     }
 
--- services/src/main/java/mine/services/ForegroundService.java
+++ services/src/main/java/mine/services/ForegroundService.java
@@ -1,5 +1,8 @@
 package mine.services;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.app.Service;
 import android.content.Intent;
 import android.os.Handler;
@@ -8,18 +11,94 @@ import android.os.Message;
 import android.os.Messenger;
 import android.os.RemoteException;
 import android.support.annotation.Nullable;
+import android.support.v4.app.NotificationCompat;
+
+import java.util.Objects;
 
 /**
  * Created by Administrator on 2019/1/9.
  */
 public class ForegroundService extends Service {
     public ForegroundService() {
-        System.out.println();
+        System.out.println("+++ " + getClass().getSimpleName() + ".constructor +++");
+
     }
+
+
+    @Override
+    public void onCreate() {
+        System.out.println("---- " + getClass().getSimpleName() + ".onCreate ----");
+
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        System.out.println("---- " + getClass().getSimpleName() + ".onStartCommand ----");
+
+        if (intent.getIntExtra("category", -1) == 1) {
+            stopForeground(true);
+            return START_STICKY;
+        }
+
+
+        //创建通知通道
+        NotificationManager notificationManager = getSystemService(NotificationManager.class);
+        String channelId = "c1";
+        CharSequence channelName = "cf";
+        int importance = NotificationManager.IMPORTANCE_LOW;
+        NotificationChannel notificationChannel = new NotificationChannel(channelId, channelName, importance);
+        notificationChannel.setShowBadge(false);
+        notificationManager.createNotificationChannel(notificationChannel);
+
+        //创建通知
+        Notification notification = new NotificationCompat.Builder(this, "c1")
+                .setSmallIcon(R.drawable.ic_launcher_foreground)
+                .setContentTitle("ttt")
+                .setContentText("cccc")
+                .setSubText("ssss")
+                .build();
+
+        //启动前台服务
+        startForeground(1, notification);
+
+        return START_STICKY;
+    }
+
+
+    @Override
+    public void onDestroy() {
+        System.out.println("---- " + getClass().getSimpleName() + ".onDestroy ----");
+        System.out.println(Thread.currentThread());
+    }
+
 
     @Nullable
     @Override
     public IBinder onBind(Intent intent) {
+        System.out.println("---- " + getClass().getSimpleName() + ".onBind ----");
         return null;
     }
+
+
+    @Override
+    public boolean onUnbind(Intent intent) {
+        System.out.println("---- " + getClass().getSimpleName() + ".onUnbind ----");
+        return true;
+    }
+
+
+    @Override
+    public void onRebind(Intent intent) {
+        System.out.println("---- " + getClass().getSimpleName() + ".onRebind ----");
+        super.onRebind(intent);
+    }
+
+
+    @Override
+    public void onTaskRemoved(Intent rootIntent) {
+        System.out.println("---- " + getClass().getSimpleName() + ".onTaskRemoved ----");
+        super.onTaskRemoved(rootIntent);
+    }
+
+
 }
