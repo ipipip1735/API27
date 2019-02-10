bc964c4de2ba4448bcefafa1b75479ba12c0f0dd
ipipip1735 <ipipip1735@163.com>
2019-02-10 17:29:55 +0800

    Update Media

--- media/src/main/java/mine/media/AudioActivity.java
+++ media/src/main/java/mine/media/AudioActivity.java
@@ -4,12 +4,17 @@ import android.media.AudioAttributes;
 import android.media.AudioDeviceInfo;
 import android.media.AudioFocusRequest;
 import android.media.AudioManager;
+import android.media.AudioPlaybackConfiguration;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
 import java.sql.SQLOutput;
+import java.util.List;
+import java.util.Random;
 
 import static android.media.AudioManager.ADJUST_RAISE;
 import static android.media.AudioManager.AUDIOFOCUS_REQUEST_FAILED;
@@ -33,18 +38,19 @@ import static android.media.AudioManager.STREAM_MUSIC;
  */
 public class AudioActivity extends AppCompatActivity {
 
-    MediaPlayer mediaPlayer;
+    int n;
     AudioFocusRequest audioFocusRequest;
-    private AudioAttributes audioAttributes = new AudioAttributes.Builder()
-            .setUsage(AudioAttributes.USAGE_MEDIA)
-            .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
-            .build();;
+    MediaPlayer mediaPlayer;
+    private AudioAttributes audioAttributes;
+    AudioManager audioManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        System.out.println("*********  " + getClass().getSimpleName() + ".onStart  *********");
+        System.out.println("*********  " + getClass().getSimpleName() + ".onCreate  *********");
         setContentView(R.layout.activity_audio);
+
+
 
     }
 
@@ -143,8 +149,8 @@ public class AudioActivity extends AppCompatActivity {
 
 //        //获取音量，音量最大值，音量最小值
         System.out.println("getStreamVolume is " + audioManager.getStreamVolume(STREAM_MUSIC));
-//        System.out.println("getStreamMaxVolume is " + audioManager.getStreamMaxVolume(STREAM_MUSIC));
-//        System.out.println("getStreamMinVolume is " + audioManager.getStreamMinVolume(STREAM_MUSIC));
+        System.out.println("getStreamMaxVolume is " + audioManager.getStreamMaxVolume(STREAM_MUSIC));
+        System.out.println("getStreamMinVolume is " + audioManager.getStreamMinVolume(STREAM_MUSIC));
 //
 //        //音量转换为分贝
 //        float db = audioManager.getStreamVolumeDb(STREAM_MUSIC, 15, AudioDeviceInfo.TYPE_TELEPHONY);
@@ -159,6 +165,7 @@ public class AudioActivity extends AppCompatActivity {
 
         AudioManager audioManager = getSystemService(AudioManager.class);
 
+        //调整音量
 //        int volume = audioManager.getStreamVolume(STREAM_MUSIC);
 //        System.out.println(volume);
 //        audioManager.setStreamVolume(STREAM_MUSIC, ++volume, FLAG_PLAY_SOUND);
@@ -167,19 +174,35 @@ public class AudioActivity extends AppCompatActivity {
 //        audioManager.adjustStreamVolume(STREAM_MUSIC, ADJUST_RAISE, FLAG_SHOW_UI);
 
 
+
+
     }
 
     public void prepare(View view) {
         System.out.println("~~button.prepare~~");
+
         //创建属性
+//        audioAttributes = new AudioAttributes.Builder()
+//                .setUsage(AudioAttributes.USAGE_MEDIA)
+//                .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
+//                .build();
+//        mediaPlayer = new MediaPlayer();
+//        mediaPlayer.setAudioAttributes(audioAttributes);//设置属性，可选的
+
+
+
+
+
+
+
 
 
         //创建请求对象
-//        audioFocusRequest = new AudioFocusRequest.Builder(AudioManager.AUDIOFOCUS_GAIN)
-        audioFocusRequest = new AudioFocusRequest.Builder(AudioManager.AUDIOFOCUS_GAIN_TRANSIENT)
-                .setAudioAttributes(audioAttributes)
-//                .setAcceptsDelayedFocusGain(true)
-//                .setWillPauseWhenDucked(true)
+        audioFocusRequest = new AudioFocusRequest.Builder(AudioManager.AUDIOFOCUS_GAIN)
+//        audioFocusRequest = new AudioFocusRequest.Builder(AudioManager.AUDIOFOCUS_GAIN_TRANSIENT)
+//        audioFocusRequest = new AudioFocusRequest.Builder(AudioManager.AUDIOFOCUS_GAIN_TRANSIENT_MAY_DUCK)
+//        audioFocusRequest = new AudioFocusRequest.Builder(AudioManager.AUDIOFOCUS_GAIN_TRANSIENT_EXCLUSIVE)
+//                .setAudioAttributes(audioAttributes)//设置属性，可选的
                 .setOnAudioFocusChangeListener(new AudioManager.OnAudioFocusChangeListener() {
                     @Override
                     public void onAudioFocusChange(int focusChange) {
@@ -202,91 +225,14 @@ public class AudioActivity extends AppCompatActivity {
                 })
                 .build();
 
-//        mediaPlayer = new MediaPlayer();
-//        mediaPlayer.setAudioAttributes(audioAttributes);
-
 
 
     }
 
     public void request(View view) {
         System.out.println("~~button.request~~");
-        AudioManager audioManager = getSystemService(AudioManager.class);
-        int focus = audioManager.requestAudioFocus(audioFocusRequest);
-        switch(focus) {
-            case AUDIOFOCUS_REQUEST_FAILED:
-                System.out.println("focus is AUDIOFOCUS_REQUEST_FAILED");
-                break;
-            case AUDIOFOCUS_REQUEST_GRANTED:
-                System.out.println("focus is AUDIOFOCUS_REQUEST_GRANTED");
-                break;
-            case AUDIOFOCUS_REQUEST_DELAYED:
-                System.out.println("focus is AUDIOFOCUS_REQUEST_DELAYED");
-                break;
-            default:
-                System.out.println("focus is unknown!");
 
-        }
-
-    }
-
-    public void reloading(View view) {
-        System.out.println("~~button.reloading~~");
-
-    }
-
-
-    public void abandon(View view) {
-        System.out.println("~~button.abandon~~");
-
-        AudioManager audioManager = getSystemService(AudioManager.class);
-        int focus = audioManager.abandonAudioFocusRequest(audioFocusRequest);
-        switch(focus) {
-            case AUDIOFOCUS_REQUEST_FAILED:
-                System.out.println("focus is AUDIOFOCUS_REQUEST_FAILED");
-                break;
-            case AUDIOFOCUS_REQUEST_GRANTED:
-                System.out.println("focus is AUDIOFOCUS_REQUEST_GRANTED");
-                break;
-            default:
-                System.out.println("focus is unknown!");
-
-        }
-    }
-
-
-    public void del(View view) {
-        System.out.println("~~button.del~~");
-
-
-        AudioFocusRequest audioFocusRequest = new AudioFocusRequest.Builder(AudioManager.AUDIOFOCUS_GAIN)
-                .setAudioAttributes(audioAttributes)
-//                .setAcceptsDelayedFocusGain(true)
-//                .setWillPauseWhenDucked(true)
-                .setOnAudioFocusChangeListener(new AudioManager.OnAudioFocusChangeListener() {
-                    @Override
-                    public void onAudioFocusChange(int focusChange) {
-                        System.out.println("~~onAudioFocusChange~~");
-                        switch (focusChange) {
-                            case AudioManager.AUDIOFOCUS_GAIN:
-                                System.out.println("focusChange2 is AUDIOFOCUS_GAIN");
-                                break;
-                            case AudioManager.AUDIOFOCUS_LOSS:
-                                System.out.println("focusChange2 is AUDIOFOCUS_LOSS");
-                                break;
-                            case AudioManager.AUDIOFOCUS_LOSS_TRANSIENT:
-                                System.out.println("focusChange2 is AUDIOFOCUS_LOSS_TRANSIENT");
-                                break;
-                            case AudioManager.AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK:
-                                System.out.println("focusChange2 is AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK");
-                                break;
-                        }
-                    }
-                })
-                .build();
-
-
-
+        //申请焦点
         AudioManager audioManager = getSystemService(AudioManager.class);
         int focus = audioManager.requestAudioFocus(audioFocusRequest);
         switch (focus) {
@@ -304,6 +250,136 @@ public class AudioActivity extends AppCompatActivity {
 
         }
 
+    }
+
+
+
+    public void abandon(View view) {
+        System.out.println("~~button.abandon~~");
+
+        //放弃焦点
+        AudioManager audioManager = getSystemService(AudioManager.class);
+        int focus = audioManager.abandonAudioFocusRequest(audioFocusRequest);
+        switch (focus) {
+            case AUDIOFOCUS_REQUEST_FAILED:
+                System.out.println("focus is AUDIOFOCUS_REQUEST_FAILED");
+                break;
+            case AUDIOFOCUS_REQUEST_GRANTED:
+                System.out.println("focus is AUDIOFOCUS_REQUEST_GRANTED");
+                break;
+            default:
+                System.out.println("focus is unknown!");
+        }
+    }
+
+
+    public void del(View view) {
+        System.out.println("~~button.del~~");
+
+
+        n = new Random().nextInt(100);
+        System.out.println("id is " + n);
+
+        AudioFocusRequest audioFocusRequest = new AudioFocusRequest.Builder(AudioManager.AUDIOFOCUS_GAIN)
+                .setOnAudioFocusChangeListener(new AudioManager.OnAudioFocusChangeListener() {
+
+                    @Override
+                    public void onAudioFocusChange(int focusChange) {
+                        System.out.println("~~onAudioFocusChange" + n + "~~");
+                        switch (focusChange) {
+                            case AudioManager.AUDIOFOCUS_GAIN:
+                                System.out.println("focusChange" + n + " is AUDIOFOCUS_GAIN");
+                                break;
+                            case AudioManager.AUDIOFOCUS_LOSS:
+                                System.out.println("focusChange" + n + " is AUDIOFOCUS_LOSS");
+                                break;
+                            case AudioManager.AUDIOFOCUS_LOSS_TRANSIENT:
+                                System.out.println("focusChange" + n + " is AUDIOFOCUS_LOSS_TRANSIENT");
+                                break;
+                            case AudioManager.AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK:
+                                System.out.println("focusChange" + n + " is AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK");
+                                break;
+                        }
+                    }
+                })
+                .build();
+
+
+        AudioManager audioManager = getSystemService(AudioManager.class);
+        int focus = audioManager.requestAudioFocus(audioFocusRequest);
+        switch (focus) {
+            case AUDIOFOCUS_REQUEST_FAILED:
+                System.out.println("focus is AUDIOFOCUS_REQUEST_FAILED");
+                break;
+            case AUDIOFOCUS_REQUEST_GRANTED:
+                System.out.println("focus is AUDIOFOCUS_REQUEST_GRANTED");
+                break;
+            case AUDIOFOCUS_REQUEST_DELAYED:
+                System.out.println("focus is AUDIOFOCUS_REQUEST_DELAYED");
+                break;
+            default:
+                System.out.println("focus is unknown!");
+        }
+
+
+
+
+
 
     }
+
+
+
+    public void delay(View view) {
+        System.out.println("~~button.delay~~");
+
+
+        //延迟批准
+        AudioFocusRequest audioFocusRequest = new AudioFocusRequest.Builder(AudioManager.AUDIOFOCUS_GAIN)
+                .setAcceptsDelayedFocusGain(true) //支持延迟
+                .setOnAudioFocusChangeListener(new AudioManager.OnAudioFocusChangeListener() {
+                    @Override
+                    public void onAudioFocusChange(int focusChange) {
+                        System.out.println("~~onAudioFocusChange~~");
+                        switch (focusChange) {
+                            case AudioManager.AUDIOFOCUS_GAIN:
+                                System.out.println("focusChange is AUDIOFOCUS_GAIN");
+                                break;
+                            case AudioManager.AUDIOFOCUS_LOSS:
+                                System.out.println("focusChange is AUDIOFOCUS_LOSS");
+                                break;
+                            case AudioManager.AUDIOFOCUS_LOSS_TRANSIENT:
+                                System.out.println("focusChange is AUDIOFOCUS_LOSS_TRANSIENT");
+                                break;
+                            case AudioManager.AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK:
+                                System.out.println("focusChange is AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK");
+                                break;
+                        }
+                    }
+                })
+                .build();
+
+        AudioManager audioManager = getSystemService(AudioManager.class);
+        int focus = audioManager.requestAudioFocus(audioFocusRequest);
+        switch (focus) {
+            case AUDIOFOCUS_REQUEST_FAILED:
+                System.out.println("focus is AUDIOFOCUS_REQUEST_FAILED");
+                break;
+            case AUDIOFOCUS_REQUEST_GRANTED:
+                System.out.println("focus is AUDIOFOCUS_REQUEST_GRANTED");
+                break;
+            case AUDIOFOCUS_REQUEST_DELAYED:
+                System.out.println("focus is AUDIOFOCUS_REQUEST_DELAYED"); //延迟了
+                break;
+            default:
+                System.out.println("focus is unknown!");
+        }
+    }
+
+
+
+
+
+
+
 }
