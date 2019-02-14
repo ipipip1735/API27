b75dd1096857deb7fd5ba045b0736d247f1dd123
ipipip1735 <ipipip1735@163.com>
2019-02-14 17:24:33 +0800

    Update View

--- view/src/main/java/mine/view/SurfaceViewActivity.java
+++ view/src/main/java/mine/view/SurfaceViewActivity.java
@@ -2,14 +2,11 @@ package mine.view;
 
 import android.graphics.Canvas;
 import android.graphics.Paint;
-import android.graphics.PorterDuff;
 import android.graphics.Rect;
-import android.graphics.drawable.ShapeDrawable;
-import android.graphics.drawable.shapes.OvalShape;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.View;
@@ -23,7 +20,9 @@ import java.util.Random;
 public class SurfaceViewActivity extends AppCompatActivity {
 
     private SurfaceHolder surfaceHolder;
+    private SurfaceView surfaceView;
     private Thread thread;
+    final Random random = new Random();
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -33,14 +32,16 @@ public class SurfaceViewActivity extends AppCompatActivity {
 
         //方式一：窗口模式
         setContentView(R.layout.activity_surface);
-        SurfaceView surfaceView = (SurfaceView) findViewById(R.id.msfv);
+        surfaceView = (SurfaceView) findViewById(R.id.msfv);
+        surfaceView.setZOrderOnTop(true);//设置顺序策略
+//        surfaceView.setZOrderMediaOverlay(true);//置还顺序策略
+//        surfaceView.setSecure(false);
+
         surfaceHolder = surfaceView.getHolder();
         surfaceHolder.addCallback(new SurfaceHolder.Callback() {
             @Override
             public void surfaceCreated(SurfaceHolder holder) {
                 System.out.println("~~~~~~~  " + getClass().getSimpleName() + ".surfaceCreated  ~~~~~~~");
-
-                final Random random = new Random();
 
 
                 thread = new Thread(new Runnable() {
@@ -79,6 +80,10 @@ public class SurfaceViewActivity extends AppCompatActivity {
             public void surfaceChanged(SurfaceHolder holder, int frmt, int w, int h) {
                 System.out.println("~~~~~~~  " + getClass().getSimpleName() + ".surfaceChanged  ~~~~~~~");
 
+                System.out.println("holder is " + holder);
+                System.out.println("frmt is " + frmt);
+                System.out.println("h is " + h);
+                System.out.println("w is " + w);
 
             }
 
@@ -102,28 +107,71 @@ public class SurfaceViewActivity extends AppCompatActivity {
     public void start(View view) {
         System.out.println("*********  button.start  *********");
 
-        thread.start();
+//        thread.start();
+
+        //设置屏幕常亮
+//        surfaceHolder.setKeepScreenOn(true);
+
 
     }
 
     public void stop(View view) {
         System.out.println("*********  button.stop  *********");
 
-        thread.interrupt();
+//        thread.interrupt();
+
+        System.out.println(surfaceHolder.getSurfaceFrame());
+//        surfaceHolder.setFixedSize(5000, 5000);
+        System.out.println(surfaceHolder.getSurfaceFrame());
+
     }
 
     public void rect(View view) {
         System.out.println("*********  button.rect  *********");
+        System.out.println(surfaceHolder.getSurfaceFrame());
 
 
-        Random random = new Random();
+
+        //渲染
+        surfaceView.setZOrderMediaOverlay(true);
         Canvas canvas = surfaceHolder.lockCanvas();
         Paint p = new Paint();
         p.setColor(getResources().getColor(R.color.AliceBlue, null));
         canvas.drawARGB(random.nextInt(255), random.nextInt(255), random.nextInt(255), random.nextInt(255));
-        canvas.drawCircle(100f, 100f, 100f, p);
-        canvas.drawColor(getResources().getColor(R.color.MediumSeaGreen, null), PorterDuff.Mode.DST_OVER);
+//        canvas.drawCircle(5000f, 5000f, 5000f, p);
+        canvas.drawRect(0, 0, 1700, 1700, p);
         surfaceHolder.unlockCanvasAndPost(canvas);
+
+
+        //测试混合模式
+//        Random random = new Random();
+//        Paint p = new Paint();
+//        p.setColor(getResources().getColor(R.color.AliceBlue, null));
+//        Canvas canvas = surfaceHolder.lockCanvas(new Rect(0,0, 100, 100));
+//        canvas.drawCircle(100f, 100f, 100f, p);
+//        canvas.drawColor(getResources().getColor(R.color.MediumSeaGreen, null),
+//                PorterDuff.Mode.DST);//使用DST模式
+//        surfaceHolder.unlockCanvasAndPost(canvas);
+
+
+        //填充颜色
+//        Random random = new Random();
+//        Canvas canvas = surfaceHolder.lockCanvas(new Rect(0,0, 100, 100));
+//        canvas.drawARGB(random.nextInt(255),random.nextInt(255),random.nextInt(255),random.nextInt(255));
+//        surfaceHolder.unlockCanvasAndPost(canvas);
+
+
+        //测试固定buffer
+//        Canvas canvas = surfaceHolder.lockCanvas(new Rect(0, 0, 800, 600));
+//        Canvas canvas = surfaceHolder.lockCanvas();
+//        Drawable d = getResources().getDrawable(R.drawable.center, null);
+//        d.setBounds(200, 200, 600, 800);
+//        d.draw(canvas);
+//        surfaceHolder.unlockCanvasAndPost(canvas);
+
+
+
+
 
     }
 }
