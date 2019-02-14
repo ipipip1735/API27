e395e934b6390432e79575379c815668a2a39a53
ipipip1735 <ipipip1735@163.com>
2019-02-14 08:00:30 +0800

    Update View

--- view/src/main/java/mine/view/SurfaceViewActivity.java
+++ view/src/main/java/mine/view/SurfaceViewActivity.java
@@ -1,6 +1,7 @@
 package mine.view;
 
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.PorterDuff;
 import android.graphics.Rect;
@@ -43,34 +44,34 @@ public class SurfaceViewActivity extends AppCompatActivity {
                 final Random random = new Random();
 
 
-                thread = new Thread(new Runnable() {
-                    @Override
-                    public void run() {
-                        while (!thread.isInterrupted()) {
-                            Canvas canvas = surfaceHolder.lockCanvas();
-                            Paint p = new Paint();
-                            p.setColor(getResources().getColor(R.color.AliceBlue, null));
-                            canvas.drawARGB(random.nextInt(255), random.nextInt(255), random.nextInt(255), random.nextInt(255));
-                            canvas.drawCircle(100f, 100f, 100f, p);
-                            surfaceHolder.unlockCanvasAndPost(canvas);
-                            try {
-                                System.out.println("--");
-                                Thread.sleep(1000L);
-                            } catch (InterruptedException e) {
-                                thread.interrupt();
-                            }
-                        }
+//                thread = new Thread(new Runnable() {
+//                    @Override
+//                    public void run() {
+//                        while (!thread.isInterrupted()) {
+//                            Canvas canvas = surfaceHolder.lockCanvas();
+//                            Paint p = new Paint();
+//                            p.setColor(getResources().getColor(R.color.AliceBlue, null));
+//                            canvas.drawARGB(random.nextInt(255), random.nextInt(255), random.nextInt(255), random.nextInt(255));
+//                            canvas.drawCircle(100f, 100f, 100f, p);
+//                            surfaceHolder.unlockCanvasAndPost(canvas);
+//                            try {
+//                                System.out.println("--");
+//                                Thread.sleep(1000L);
+//                            } catch (InterruptedException e) {
+//                                thread.interrupt();
+//                            }
+//                        }
+//
+//                    }
+//                });
 
-                    }
-                });
 
-
-//                Canvas canvas = surfaceHolder.lockCanvas();
-//                Paint p = new Paint();
-//                p.setColor(getResources().getColor(R.color.AliceBlue, null));
-//                canvas.drawARGB(random.nextInt(255), random.nextInt(255), random.nextInt(255), random.nextInt(255));
-//                canvas.drawCircle(100f, 100f, 100f, p);
-//                surfaceHolder.unlockCanvasAndPost(canvas);
+                Canvas canvas = surfaceHolder.lockCanvas();
+                Paint p = new Paint();
+                p.setColor(getResources().getColor(R.color.AliceBlue, null));
+                canvas.drawARGB(random.nextInt(255), random.nextInt(255), random.nextInt(255), random.nextInt(255));
+                canvas.drawCircle(100f, 100f, 100f, p);
+                surfaceHolder.unlockCanvasAndPost(canvas);
 
 
             }
@@ -78,6 +79,10 @@ public class SurfaceViewActivity extends AppCompatActivity {
             @Override
             public void surfaceChanged(SurfaceHolder holder, int frmt, int w, int h) {
                 System.out.println("~~~~~~~  " + getClass().getSimpleName() + ".surfaceChanged  ~~~~~~~");
+
+                System.out.println("frmt is " + frmt);
+                System.out.println("w is " + w);
+                System.out.println("h is " + h);
 
 
             }
@@ -102,28 +107,91 @@ public class SurfaceViewActivity extends AppCompatActivity {
     public void start(View view) {
         System.out.println("*********  button.start  *********");
 
-        thread.start();
+//        thread.start();
 
     }
 
     public void stop(View view) {
         System.out.println("*********  button.stop  *********");
 
-        thread.interrupt();
+//        thread.interrupt();
+
+//        Random random = new Random();
+//        Paint p = new Paint();
+
+//        p.setColor(getResources().getColor(R.color.MediumSeaGreen, null));
+//        Canvas canvas = surfaceHolder.lockCanvas();
+//        canvas.drawColor(Color.BLUE);
+//        surfaceHolder.unlockCanvasAndPost(canvas);
+
+        surfaceHolder.setFixedSize(1000, 1200);
+
+
     }
 
     public void rect(View view) {
         System.out.println("*********  button.rect  *********");
 
 
-        Random random = new Random();
-        Canvas canvas = surfaceHolder.lockCanvas();
-        Paint p = new Paint();
-        p.setColor(getResources().getColor(R.color.AliceBlue, null));
-        canvas.drawARGB(random.nextInt(255), random.nextInt(255), random.nextInt(255), random.nextInt(255));
-        canvas.drawCircle(100f, 100f, 100f, p);
-        canvas.drawColor(getResources().getColor(R.color.MediumSeaGreen, null), PorterDuff.Mode.DST_OVER);
+//        Random random = new Random();
+//        Paint p = new Paint();
+//        p.setColor(getResources().getColor(R.color.MediumSeaGreen, null));
+
+
+        System.out.println(surfaceHolder.getSurfaceFrame());
+//        surfaceHolder.setFixedSize(10, 25);
+//        System.out.println(surfaceHolder.getSurfaceFrame());
+//        surfaceHolder.setSizeFromLayout();
+//        surfaceHolder.setKeepScreenOn(false);
+//        System.out.println(surfaceHolder.getSurfaceFrame());
+
+        Canvas canvas;
+
+//        canvas = surfaceHolder.lockCanvas();
+//        canvas.drawColor(Color.BLUE);
+//        surfaceHolder.unlockCanvasAndPost(canvas);
+//
+//        try {
+//            Thread.sleep(1000L);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+//
+//        canvas = surfaceHolder.lockCanvas(null);
+        canvas = surfaceHolder.lockCanvas(new Rect(100,100, 800, 600));
+        canvas.drawColor(Color.YELLOW);
         surfaceHolder.unlockCanvasAndPost(canvas);
+//
+//
+//
+//        try {
+//            Thread.sleep(1000L);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+//
+//
+//
+//        canvas = surfaceHolder.lockCanvas(null);
+////        canvas = surfaceHolder.lockCanvas(new Rect(50,50, 100, 150));
+//        canvas.drawColor(Color.RED);
+//        surfaceHolder.unlockCanvasAndPost(canvas);
+//
+//        try {
+//            Thread.sleep(1000L);
+//            System.out.println("sleep 1");
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+//
+//
+//        canvas = surfaceHolder.lockCanvas(null);
+////        canvas = surfaceHolder.lockCanvas(new Rect(50,60, 100, 160));
+//        canvas.drawColor(Color.GREEN);
+//        surfaceHolder.unlockCanvasAndPost(canvas);
+
+
+
 
     }
 }
