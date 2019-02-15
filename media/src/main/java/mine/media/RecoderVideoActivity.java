bcc7f39d585b44a3c1c6a9e889e466480ad3fe2e
ipipip1735 <ipipip1735@163.com>
2019-02-15 17:29:06 +0800

    Update Media

---
+++ media/src/main/java/mine/media/RecoderVideoActivity.java
@@ -0,0 +1,225 @@
+package mine.media;
+
+import android.content.pm.ActivityInfo;
+import android.media.MediaRecorder;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.SurfaceView;
+import android.view.View;
+
+import java.io.File;
+import java.io.IOException;
+
+import static android.os.Environment.DIRECTORY_MOVIES;
+import static android.os.Environment.DIRECTORY_MUSIC;
+
+
+/**
+ * Created by Administrator on 2019/2/1.
+ */
+public class RecoderVideoActivity extends AppCompatActivity {
+
+    MediaRecorder recorder;
+    int n;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        System.out.println("*********  " + getClass().getSimpleName() + ".onCreate  *********");
+        setContentView(R.layout.activity_recorder_video);
+        recorder = new MediaRecorder();
+
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        System.out.println("*********  " + getClass().getSimpleName() + ".onStart  *********");
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        System.out.println("*********  " + getClass().getSimpleName() + ".onRestoreInstanceState  *********");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        System.out.println("*********  " + getClass().getSimpleName() + ".onRestart  *********");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        System.out.println("*********  " + getClass().getSimpleName() + ".onResume  *********");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        System.out.println("*********  " + getClass().getSimpleName() + ".onPause  *********");
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        System.out.println("*********  " + getClass().getSimpleName() + ".onBackPressed  *********");
+    }
+
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        System.out.println("*********  " + getClass().getSimpleName() + ".onStop  *********");
+    }
+
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        System.out.println("*********  " + getClass().getSimpleName() + ".onSaveInstanceState  *********");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        System.out.println("*********  " + getClass().getSimpleName() + ".onDestroy  *********");
+        recorder.release();
+        recorder = null;
+    }
+
+
+    public void state(View view) {
+        System.out.println("~~button.state~~");
+
+//        try {
+//            System.out.println("getActiveMicrophones is " + recorder.getActiveMicrophones());
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+
+//        System.out.println("getAudioSourceMax is " + recorder.getAudioSourceMax());
+//        System.out.println("getMaxAmplitude is " + recorder.getMaxAmplitude());
+//        System.out.println("getMetrics is " + recorder.getMetrics());
+
+
+//        System.out.println("getPreferredDevice is " + recorder.getPreferredDevice());
+//        System.out.println("getRoutedDevice is " + recorder.getRoutedDevice());
+//        System.out.println("getSurface is " + recorder.getSurface());
+
+
+
+
+    }
+
+    public void init(View view) {
+        System.out.println("~~button.init~~");
+
+
+        //initial
+        recorder.setAudioSource(MediaRecorder.AudioSource.MIC); //设置音频输入设备
+        recorder.setVideoSource(MediaRecorder.VideoSource.CAMERA); //设置视频输入设备
+
+
+        //DataSourceConfigured
+        recorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP); //设置输出格式
+        recorder.setVideoEncoder(MediaRecorder.VideoEncoder.MPEG_4_SP); //设置音频编码
+        recorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB); //设置编码方式
+        File path = new File(getExternalFilesDir(DIRECTORY_MOVIES), "/rc" + n + ".mp4"); //设置输出路径
+        System.out.println(path);
+        recorder.setOutputFile(path.toString());
+
+        SurfaceView surfaceView = findViewById(R.id.surfaceView);
+        recorder.setPreviewDisplay(surfaceView.getHolder().getSurface());
+
+
+
+
+        //绑定监听器
+//        recorder.setOnErrorListener(new MediaRecorder.OnErrorListener() {
+//            @Override
+//            public void onError(MediaRecorder mr, int what, int extra) {
+//                System.out.println("~~onError~~");
+//                System.out.println("what is " + what);
+//                System.out.println("extra is " + extra);
+//            }
+//        });
+//
+//        try {
+//            recorder.setOnInfoListener(new MediaRecorder.OnInfoListener() {
+//                @Override
+//                public void onInfo(MediaRecorder mr, int what, int extra) {
+//                    System.out.println("~~onInfo~~");
+//                    System.out.println("what is " + what);
+//                    System.out.println("extra is " + extra);
+//
+//                }
+//            });
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+
+    }
+
+    public void prepare(View view) {
+        System.out.println("~~button.prepare~~");
+
+        try {
+            recorder.prepare();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public void start(View view) {
+        System.out.println("~~button.start~~");
+        recorder.start();   // Recording is now started
+
+
+    }
+
+
+    public void stop(View view) {
+        System.out.println("~~button.stop~~");
+
+        recorder.stop();
+
+
+    }
+
+    public void pause(View view) {
+        System.out.println("~~button.pause~~");
+        recorder.pause();
+    }
+
+    public void resume(View view) {
+        System.out.println("~~button.resume~~");
+        recorder.resume();
+    }
+
+    public void reset(View view) {
+        System.out.println("~~button.reset~~");
+        recorder.reset();   // You can reuse the object by going back to setAudioSource() step
+
+    }
+
+
+    public void release(View view) {
+        System.out.println("~~button.release~~");
+
+        recorder.release(); // Now the object cannot be reused
+
+
+    }
+
+
+    public void query(View view) {
+        System.out.println("~~button.query~~");
+
+
+    }
+
+}
---
+++ media/src/main/java/mine/media/VideoSurfaceActivity.java
@@ -0,0 +1,194 @@
+package mine.media;
+
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Surface;
+import android.view.SurfaceView;
+import android.view.View;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+import java.io.IOException;
+import java.util.Objects;
+
+
+/**
+ * Created by Administrator on 2019/2/15.
+ */
+public class VideoSurfaceActivity extends AppCompatActivity {
+
+    MediaPlayer mediaPlayer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        System.out.println("*********  " + getClass().getSimpleName() + ".onCreate  *********");
+        setContentView(R.layout.activity_vedio_surface);
+
+
+        mediaPlayer = new MediaPlayer();
+
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        System.out.println("*********  " + getClass().getSimpleName() + ".onStart  *********");
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        System.out.println("*********  " + getClass().getSimpleName() + ".onRestoreInstanceState  *********");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        System.out.println("*********  " + getClass().getSimpleName() + ".onRestart  *********");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        System.out.println("*********  " + getClass().getSimpleName() + ".onResume  *********");
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        System.out.println("*********  " + getClass().getSimpleName() + ".onPause  *********");
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        System.out.println("*********  " + getClass().getSimpleName() + ".onBackPressed  *********");
+    }
+
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        System.out.println("*********  " + getClass().getSimpleName() + ".onStop  *********");
+    }
+
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        System.out.println("*********  " + getClass().getSimpleName() + ".onSaveInstanceState  *********");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        System.out.println("*********  " + getClass().getSimpleName() + ".onDestroy  *********");
+
+        if (Objects.nonNull(mediaPlayer)) {
+            mediaPlayer.release();
+            mediaPlayer = null;
+        }
+
+    }
+
+
+    public void state(View view) {
+        System.out.println("~~button.state~~");
+
+
+    }
+
+    public void init(View view) {
+        System.out.println("~~button.init~~");
+
+        Uri mp4 = Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.rbt);
+        try {
+            mediaPlayer.setDataSource(this, mp4);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        SurfaceView surfaceView = findViewById(R.id.surfaceView);
+        mediaPlayer.setDisplay(surfaceView.getHolder());//设置SurfaceHolder
+        mediaPlayer.setScreenOnWhilePlaying(true); //让屏幕不要自动锁定
+        //错误监听器
+        mediaPlayer.setOnErrorListener(new MediaPlayer.OnErrorListener() {
+            @Override
+            public boolean onError(MediaPlayer mp, int what, int extra) {
+                System.out.println(what);
+                System.out.println(extra);
+                return true;
+            }
+        });
+
+
+    }
+
+    public void prepare(View view) {
+        System.out.println("~~button.prepare~~");
+
+        try {
+            mediaPlayer.prepare();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void start(View view) {
+        System.out.println("~~button.start~~");
+
+        if (!mediaPlayer.isPlaying()) mediaPlayer.start();
+
+    }
+
+
+    public void stop(View view) {
+        System.out.println("~~button.stop~~");
+
+        mediaPlayer.stop();
+
+    }
+
+    public void pause(View view) {
+        System.out.println("~~button.pause~~");
+
+        if (mediaPlayer.isPlaying()) {
+            mediaPlayer.pause();
+            mediaPlayer.setScreenOnWhilePlaying(false); //视频暂停后不要让屏幕一直亮着
+        }
+
+    }
+
+    public void resume(View view) {
+        System.out.println("~~button.resume~~");
+
+        if (!mediaPlayer.isPlaying()) {
+            mediaPlayer.pause();
+            mediaPlayer.setScreenOnWhilePlaying(true); //视频暂停后不要让屏幕一直亮着
+        }
+    }
+
+
+    public void suspend(View view) {
+        System.out.println("~~button.suspend~~");
+
+        mediaPlayer.release();
+        mediaPlayer = null;
+
+    }
+
+
+    public void query(View view) {
+        System.out.println("~~button.query~~");
+    }
+
+    public void seek(View view) {
+        System.out.println("~~button.seek~~");
+
+    }
+}
