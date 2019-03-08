033ad3c9209f56448dfbb5c8bac435225bc02581
ipipip1735 <ipipip1735@163.com>
2019-03-08 08:11:24 +0800

    Update View

---
+++ view/src/main/java/mine/view/CanvasActivity.java
@@ -0,0 +1,63 @@
+package mine.view;
+
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+import static android.graphics.Path.Direction.CCW;
+
+/**
+ * Created by Administrator on 2019/3/7.
+ */
+
+public class CanvasActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        System.out.println("*********  " + getClass().getSimpleName() + ".onCreate  *********");
+        super.onCreate(savedInstanceState);
+
+        SurfaceView surfaceView = new SurfaceView(this);
+        setContentView(surfaceView);
+
+        surfaceView.getHolder().addCallback(new SurfaceHolder.Callback() {
+            @Override
+            public void surfaceCreated(SurfaceHolder holder) {
+                Canvas canvas = holder.lockCanvas();
+                Paint paint = new Paint();
+                paint.setColor(getColor(R.color.red));
+                paint.setStyle(Paint.Style.STROKE);
+
+                Path path = new Path();
+                path.addRect(100, 100, 500, 300, CCW);//矩形路径
+                path.addArc(100, 100, 500, 300, 0f, 90f);
+                canvas.drawPath(path, paint);
+
+
+                holder.unlockCanvasAndPost(canvas);
+
+            }
+
+            @Override
+            public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+            }
+
+            @Override
+            public void surfaceDestroyed(SurfaceHolder holder) {
+
+            }
+        });
+
+
+    }
+}
+
+
+
+
