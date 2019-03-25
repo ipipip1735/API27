2533cd7d9f88ba64fcfdfb611994d89217acbfa6
ipipip1735 <ipipip1735@163.com>
2019-03-25 08:07:12 +0800

    Update Animation

--- animation/src/main/java/mine/animation/WindowOneTransitionActivity.java
+++ animation/src/main/java/mine/animation/WindowOneTransitionActivity.java
@@ -43,29 +43,88 @@ public class WindowOneTransitionActivity extends AppCompatActivity {
         super.onCreate(bundle);
         System.out.println("*********  " + getClass().getSimpleName() + ".onCreate  *********");
 
-//        window.requestFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_window_one);
+
+
+        long duration = 2000L;
 
         Window window = getWindow();
-//        window.requestFeature(Window.FEATURE_CONTENT_TRANSITIONS);
+//        window.requestFeature(Window.FEATURE_NO_TITLE);
+//        window.requestFeature(Window.FEATURE_CONTENT_TRANSITIONS); //启用变换
 
 
-        window.setEnterTransition(new Slide(TOP).setDuration(5000L)); //退出动画
-        window.setExitTransition(new Slide(LEFT).setDuration(5000L)); //退出动画
-//        window.setReenterTransition(new Fade().setDuration(5000L)); //返回动画
+        //创建转换对象
+        Transition fade = new Fade().setDuration(duration);
+        fade.addListener(new TransitionListenerAdapter() {
+            @Override
+            public void onTransitionStart(Transition transition) {
+                System.out.println("~~Fade.onTransitionStart~~");
+                System.out.println(transition.getTransitionValues(findViewById(R.id.imageViewShared), true));
+            }
+
+            @Override
+            public void onTransitionEnd(Transition transition) {
+                System.out.println("~~Fade.onTransitionEnd~~");
+                System.out.println(transition.getTransitionValues(findViewById(R.id.imageViewShared), true));
+            }
+        });
+        Transition explode = new Explode().setDuration(duration);
+        explode.addListener(new TransitionListenerAdapter() {
+            @Override
+            public void onTransitionStart(Transition transition) {
+                System.out.println("~~Explode.onTransitionStart~~");
+            }
+
+            @Override
+            public void onTransitionEnd(Transition transition) {
+                System.out.println("~~Explode.onTransitionEnd~~");
+            }
+        });
+        Transition slide = new Slide(LEFT).setDuration(duration);
+        slide.addListener(new TransitionListenerAdapter() {
+            @Override
+            public void onTransitionStart(Transition transition) {
+                System.out.println("~~Slide.onTransitionStart~~");
+            }
+
+            @Override
+            public void onTransitionEnd(Transition transition) {
+                System.out.println("~~Slide.onTransitionEnd~~");
+            }
+        });
+        Transition changesBounds = new ChangeBounds().setDuration(duration);
+        changesBounds.addListener(new TransitionListenerAdapter() {
+            @Override
+            public void onTransitionEnd(Transition transition) {
+                System.out.println("~~ChangeBounds.onTransitionEnd~~");
+            }
+
+            @Override
+            public void onTransitionStart(Transition transition) {
+                System.out.println("~~ChangeBounds.onTransitionStart~~");
+            }
+        });
 
 
-
-//        window.setAllowEnterTransitionOverlap(false);
-        window.setAllowReturnTransitionOverlap(false);
-
-        System.out.println(getClass().getSimpleName() + " is " + window);
-        System.out.println("getEnterTransition is "  + window.getEnterTransition());
-        System.out.println("getExitTransition is "  + window.getExitTransition());
-        System.out.println("getReturnTransition is "  + window.getReturnTransition());
-        System.out.println("getReenterTransition is "  + window.getReenterTransition());
+        //排除状态条和导航条
+        slide.excludeTarget(R.id.action_bar_container, true);
+        slide.excludeTarget(android.R.id.statusBarBackground, true);
+        slide.excludeTarget(android.R.id.navigationBarBackground, true);
 
 
-        setContentView(R.layout.activity_window_one);
+        //设置转换对象
+        window.setExitTransition(slide); //退出变换
+        window.setReenterTransition(slide); //重进入变换
+//        window.setAllowReturnTransitionOverlap(false); //返回播放模式，false为顺序播放，默认值true为同时播放
+        window.setTransitionBackgroundFadeDuration(duration);
+
+
+        //设置共享组件转换对象（一般不会使用SharedElementExitTransition/SharedElementReenterTransition）
+//        window.setSharedElementExitTransition(fade);  //共享组件退出变换
+//        window.setSharedElementReenterTransition(changesBounds); //共享组件重进入变换
+//        window.setSharedElementsUseOverlay(false); //共享转换禁用遮罩层
+
+
     }
 
     @Override
@@ -148,6 +207,58 @@ public class WindowOneTransitionActivity extends AppCompatActivity {
         startActivity(intent, options.toBundle());
 
 
+        //打印变换信息
+        Window window = getWindow();
+        System.out.println(getClass().getSimpleName() + " is " + window);
+        System.out.println("getEnterTransition is " + window.getEnterTransition());
+        System.out.println("getExitTransition is " + window.getExitTransition());
+        System.out.println("getReturnTransition is " + window.getReturnTransition());
+        System.out.println("getReenterTransition is " + window.getReenterTransition());
+
+
     }
+
+    public void sharedStart(View view) {
+        System.out.println("********sharedStart******");
+
+        ImageView imageView = findViewById(R.id.imageViewShared);
+
+        Intent intent = new Intent(this, WindowTwoTransitionActivity.class);
+        ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(this, imageView, "shared");
+        startActivity(intent, options.toBundle());
+
+
+        /**
+         * 如果使用了SharedElementExitTransition动画，需要在启动Activity后修改组件属性
+         */
+//        imageView.setX(imageView.getX() - 150f);
+//        imageView.setY(imageView.getY() - 450f);
+
+//        ViewGroup.LayoutParams layoutParams = imageView.getLayoutParams();
+//        layoutParams.width += 150f;
+//        layoutParams.height += 150f;
+//        imageView.setLayoutParams(layoutParams);
+//
+
+
+        //如果SharedElementExitTransition使用ChangeBounds就需要父View重新布局
+//        View parent = (View) imageView.getParent();
+//        int widthSpec = View.MeasureSpec.makeMeasureSpec(parent.getWidth(), View.MeasureSpec.EXACTLY);
+//        int heightSpec = View.MeasureSpec.makeMeasureSpec(parent.getHeight(), View.MeasureSpec.EXACTLY);
+//        parent.measure(widthSpec, heightSpec);
+//        parent.layout(parent.getLeft(), parent.getTop(), parent.getRight(), parent.getBottom());
+
+
+        //如果SharedElementExitTransition使用Fade就需要修改父View的可见性
+//        View parent = (View) imageView.getParent();
+//        parent.setVisibility(View.INVISIBLE);
+
+    }
+
+    public void sharedStop(View view) {
+        System.out.println("********sharedStop******");
+
+    }
+
 
 }
--- animation/src/main/java/mine/animation/WindowTwoTransitionActivity.java
+++ animation/src/main/java/mine/animation/WindowTwoTransitionActivity.java
@@ -3,10 +3,15 @@ package mine.animation;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.transition.ChangeBounds;
+import android.transition.Explode;
 import android.transition.Fade;
 import android.transition.Slide;
+import android.transition.Transition;
+import android.transition.TransitionListenerAdapter;
 import android.view.View;
 import android.view.Window;
+import android.widget.ImageView;
 
 import static android.view.Gravity.LEFT;
 import static android.view.Gravity.RIGHT;
@@ -23,34 +28,102 @@ public class WindowTwoTransitionActivity extends AppCompatActivity {
     protected void onCreate(Bundle bundle) {
         super.onCreate(bundle);
         System.out.println("*********  " + getClass().getSimpleName() + ".onCreate  *********");
+        setContentView(R.layout.activity_window_two);
+
+        long duration = 2000L;
+
 
         Window window = getWindow();
-
-//        window.setEnterTransition(new Fade().setDuration(5000L)); //进入动画
-//        getWindow().setExitTransition(new Slide(RIGHT).setDuration(5000L)); //返回动画
-
-
-
-
-
 //        window.requestFeature(Window.FEATURE_NO_TITLE);
-//        window.requestFeature(Window.FEATURE_CONTENT_TRANSITIONS);
+//        window.requestFeature(Window.FEATURE_CONTENT_TRANSITIONS); //启用变换
+
+        //创建转换对象
+        Transition fade = new Fade().setDuration(duration);
+        fade.addListener(new TransitionListenerAdapter() {
+            @Override
+            public void onTransitionStart(Transition transition) {
+                System.out.println("~~Fade.onTransitionStart~~");
+            }
+            @Override
+            public void onTransitionEnd(Transition transition) {
+                System.out.println("~~Fade.onTransitionEnd~~");
+            }
+
+        });
+        Transition explode = new Explode().setDuration(duration);
+        explode.addListener(new TransitionListenerAdapter() {
+            @Override
+            public void onTransitionStart(Transition transition) {
+                System.out.println("~~Explode.onTransitionStart~~");
+            }
+            @Override
+            public void onTransitionEnd(Transition transition) {
+                System.out.println("~~Explode.onTransitionEnd~~");
+            }
+        });
+        Transition slide = new Slide(TOP).setDuration(duration);
+        slide.addListener(new TransitionListenerAdapter() {
+            @Override
+            public void onTransitionStart(Transition transition) {
+                System.out.println("~~Slide.onTransitionStart~~");
+            }
+            @Override
+            public void onTransitionEnd(Transition transition) {
+                System.out.println("~~Slide.onTransitionEnd~~");
+            }
+        });
+        Transition changesBounds = new ChangeBounds().setDuration(duration);
+        changesBounds.addListener(new TransitionListenerAdapter() {
+            @Override
+            public void onTransitionEnd(Transition transition) {
+                System.out.println("~~ChangeBounds.onTransitionEnd~~");
+            }
+            @Override
+            public void onTransitionStart(Transition transition) {
+                System.out.println("~~ChangeBounds.onTransitionStart~~");
+            }
+        });
 
 
-//        window.setAllowEnterTransitionOverlap(false);
-//        window.setAllowReturnTransitionOverlap(false);
+
+        slide.excludeTarget(R.id.action_bar_container, true);
+        slide.excludeTarget(android.R.id.statusBarBackground, true);
+        slide.excludeTarget(android.R.id.navigationBarBackground, true);
+        explode.excludeTarget(R.id.action_bar_container, true);
+        explode.excludeTarget(android.R.id.statusBarBackground, true);
+        explode.excludeTarget(android.R.id.navigationBarBackground, true);
+
+
+
+        //设置转换对象
+        window.setEnterTransition(slide); //进入变换
+        window.setReturnTransition(explode); //返回变换
+//        window.setAllowEnterTransitionOverlap(false); //返回播放模式，false为顺序播放，默认值true为同时播放
+        window.setTransitionBackgroundFadeDuration(duration);
+
+
+        //设置共享组件转换对象
+        window.setSharedElementEnterTransition(changesBounds); //共享组件进入变换
+//        window.setSharedElementReturnTransition(changesBounds);  //共享组件返回变换，优先级高于SharedElementEnterTransition
+//        window.setSharedElementsUseOverlay(false); //共享组件转换禁用遮罩层
 
 
 
 
-        System.out.println(getClass().getSimpleName() + " is " + window);
-        System.out.println("getEnterTransition is "  + window.getEnterTransition());
-        System.out.println("getExitTransition is "  + window.getExitTransition());
-        System.out.println("getReturnTransition is "  + window.getReturnTransition());
-        System.out.println("getReenterTransition is "  + window.getReenterTransition());
 
 
-        setContentView(R.layout.activity_window_two);
+//        System.out.println(getClass().getSimpleName() + " is " + window);
+//        System.out.println("getEnterTransition is "  + window.getEnterTransition());
+//        System.out.println("getExitTransition is "  + window.getExitTransition());
+//        System.out.println("getReturnTransition is "  + window.getReturnTransition());
+//        System.out.println("getReenterTransition is "  + window.getReenterTransition());
+
+
+//        ImageView imageView = findViewById(R.id.ivTwo);
+//        imageView
+
+
+
 
 
     }
@@ -124,12 +197,16 @@ public class WindowTwoTransitionActivity extends AppCompatActivity {
     public void stop(View view) {
         System.out.println("********stop******");
 
-//        getWindow().setReturnTransition(new Slide(RIGHT).setDuration(5000L)); //返回动画
         finishAfterTransition();
+        System.out.println("--------------");
 
-
-//        finish();
-
+        //打印变换信息
+        Window window = getWindow();
+        System.out.println(getClass().getSimpleName() + " is " + window);
+        System.out.println("getEnterTransition is "  + window.getEnterTransition());
+        System.out.println("getExitTransition is "  + window.getExitTransition());
+        System.out.println("getReturnTransition is "  + window.getReturnTransition());
+        System.out.println("getReenterTransition is "  + window.getReenterTransition());
 
     }
 
