67737befef8bf2135ec99615017f5f62e898f5f9
ipipip1735 <ipipip1735@163.com>
2019-03-28 17:19:58 +0800

    Update RecyclerView

--- recyclerview/src/main/java/mine/recyclerview/MainActivity.java
+++ recyclerview/src/main/java/mine/recyclerview/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -17,19 +18,21 @@ public class MainActivity extends AppCompatActivity {
 
     private RecyclerView recyclerView;
     private RecyclerView.Adapter adapter;
-    private RecyclerView.LayoutManager layoutManager;
+    private LinearLayoutManager layoutManager;
     View target;
 
     List<String> dataset;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            System.out.println("*********  " + getClass().getSimpleName() + ".onCreate  *********");
+        super.onCreate(savedInstanceState);
+        System.out.println("*********  " + getClass().getSimpleName() + ".onCreate  *********");
         setContentView(R.layout.activity_main);
 
+        layoutManager = new LinearLayoutManager(this);
+
         dataset = new ArrayList<>(8);
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < 10; i++) {
             dataset.add("item" + i);
         }
         adapter = new RVAdapter<>(dataset);
@@ -39,7 +42,6 @@ public class MainActivity extends AppCompatActivity {
         recyclerView.setLayoutManager(layoutManager);
         recyclerView.setAdapter(adapter);
 
-        layoutManager = new LinearLayoutManager(this);
     }
 
     @Override
@@ -102,8 +104,11 @@ public class MainActivity extends AppCompatActivity {
     public void add(View view) {
         System.out.println("~~button.add~~");
 
-        dataset.add("aaa");
-        recyclerView.getAdapter().notifyItemInserted(0);
+//        dataset.add("item_" + dataset.size());
+//        recyclerView.getAdapter().notifyItemInserted(0);
+
+
+//        layoutManager.attachView();
 
     }
 
@@ -111,33 +116,115 @@ public class MainActivity extends AppCompatActivity {
     public void del(View view) {
         System.out.println("~~button.del~~");
 
-        recyclerView.removeView(target);
+//        recyclerView.removeView(target);
 //        recyclerView.removeViewAt(textView);
 
-    }
 
-    public void position(View view) {
-        System.out.println("~~button.position~~");
+        layoutManager.removeViewAt(1);
+        layoutManager.removeAllViews();
+        layoutManager.requestLayout();
 
-        recyclerView.getChildAdapterPosition(target);
-//        recyclerView.getChildLayoutPosition(target);
 
 
     }
 
-    public void unbind(View view) {
-        System.out.println("~~button.unbind~~");
+    public void info(View view) {
+        System.out.println("~~button.info~~");
+
+//        ViewGroup viewGroup = recyclerView;
+//        System.out.println("recyclerView.getChildCount is " + viewGroup.getChildCount());
+
+
+        //适配器信息
+        System.out.println("adapter.getItemCount is " + adapter.getItemCount());
+
+        //布局信息
+        System.out.println("layoutManager.getChildCount is " + layoutManager.getChildCount());
+        System.out.println("layoutManager.getItemCount is " + layoutManager.getItemCount());
+        System.out.println("layoutManager.getHeight is " + layoutManager.getHeight());
+        System.out.println("layoutManager.getWidth is " + layoutManager.getWidth());
+
+
+
+//        ViewInfo(); //获取测量值
+
+
+//        for ()
+////        System.out.println("adapter.getItemCount() is " + adapter.get);
+//        System.out.println("adapter.getItemCount() is " + adapter.getItemCount());
+//        System.out.println("adapter.getItemCount() is " + adapter.getItemCount());
+//        System.out.println("adapter.getItemCount() is " + adapter.getItemCount());
+
+
 
     }
 
-    public void reloading(View view) {
-        System.out.println("~~button.reloading~~");
+    public void find(View view) {
+        System.out.println("~~button.find~~");
+
+        //遍历可见子View，也就是遍历RecyclerView
+//        for (int i = 0; i < layoutManager.getChildCount(); i++) {
+//            TextView textView = (TextView) layoutManager.getChildAt(i);
+//            System.out.println(textView.getText());
+//
+//            textView = (TextView) layoutManager.findViewByPosition(i);
+//            System.out.println(textView.getText());
+//        }
+
+
+        //遍历所有子View
+//        for (int i = 0; i < recyclerView.getChildLayoutPosition(); i++) {
+//
+//        }
+
+//        View v = recyclerView.getLayoutManager().findViewByPosition(1);
+//        System.out.println(v);
+
 
     }
-
 
     public void query(View view) {
         System.out.println("~~button.query~~");
 
     }
+
+
+    public void modify(View view) {
+        System.out.println("~~button.modify~~");
+
+//        modifyLayout(); //修改RecyclerView布局，迫使LayoutManager增加子View
+        move();
+    }
+
+
+    /*---------------modify------------------*/
+    private void move() {
+
+//        layoutManager.moveView(0,6);
+//        layoutManager.scrollToPosition(0);
+//        recyclerView.smoothScrollToPosition(0);
+//        layoutManager.scrollToPositionWithOffset(0, 2);
+
+
+    }
+
+    private void modifyLayout() {
+
+        ViewGroup.LayoutParams layoutParams = recyclerView.getLayoutParams();
+        layoutParams.height += 150f;
+        recyclerView.setLayoutParams(layoutParams);
+
+    }
+
+    /*--------------info-------------------*/
+    private void ViewInfo() {
+
+        //获取子View尺寸（包含装饰器附加部分)
+        layoutManager.getDecoratedMeasuredHeight(layoutManager.getChildAt(0));
+        layoutManager.getDecoratedMeasuredHeight(layoutManager.getChildAt(0));
+
+    }
+    /*---------------------------------*/
+    /*---------------------------------*/
+    /*---------------------------------*/
 }
