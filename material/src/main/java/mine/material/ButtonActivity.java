4693126f9420f5915a98d01f1f3b721671eedc34
ipipip1735 <ipipip1735@163.com>
2020-06-22 18:06:54 +0800

    Add material

---
+++ material/src/main/java/mine/material/ButtonActivity.java
@@ -0,0 +1,119 @@
+package mine.material;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ButtonActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        System.out.println("*********  " + getClass().getSimpleName() + ".onCreate  *********");
+        setContentView(R.layout.activity_button);
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        System.out.println("*********  " + getClass().getSimpleName() + ".onStart  *********");
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        System.out.println("*********  " + getClass().getSimpleName() + ".onRestoreInstanceState  *********");
+
+        super.onRestoreInstanceState(savedInstanceState);
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        System.out.println("*********  " + getClass().getSimpleName() + ".onRestart  *********");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        System.out.println("*********  " + getClass().getSimpleName() + ".onResume  *********");
+    }
+
+    @Override
+    protected void onPause() {
+        System.out.println("*********  " + getClass().getSimpleName() + ".onPause  *********");
+
+        super.onPause();
+    }
+
+    @Override
+    public void onBackPressed() {
+        System.out.println("*********  " + getClass().getSimpleName() + ".onBackPressed  *********");
+
+        super.onBackPressed();
+    }
+
+
+    @Override
+    protected void onStop() {
+        System.out.println("*********  " + getClass().getSimpleName() + ".onStop  *********");
+
+        super.onStop();
+    }
+
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        System.out.println("*********  " + getClass().getSimpleName() + ".onSaveInstanceState  *********");
+
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    protected void onDestroy() {
+        System.out.println("*********  " + getClass().getSimpleName() + ".onDestroy  *********");
+
+        super.onDestroy();
+    }
+
+
+    public void start(View view) {
+        System.out.println("~~button.start~~");
+
+    }
+
+
+    public void stop(View view) {
+        System.out.println("~~button.stop~~");
+
+    }
+
+    public void bind(View view) {
+        System.out.println("~~button.bind~~");
+
+    }
+
+    public void unbind(View view) {
+        System.out.println("~~button.unbind~~");
+
+    }
+
+    public void reloading(View view) {
+        System.out.println("~~button.reloading~~");
+
+    }
+
+
+    public void del(View view) {
+        System.out.println("~~button.del~~");
+
+    }
+
+
+    public void query(View view) {
+        System.out.println("~~button.query~~");
+
+    }
+}
